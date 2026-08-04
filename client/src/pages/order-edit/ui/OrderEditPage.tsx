211b0d05d9ec88965983704a28dc440053ddc684
tingoll <roma.oliynyk83@gmail.com>
2026-08-04 15:28:58 +0300

    client: add order breadcrumbs

--- client/src/pages/order-edit/ui/OrderEditPage.tsx
+++ client/src/pages/order-edit/ui/OrderEditPage.tsx
@@ -1,18 +1,33 @@
 import { css } from '@emotion/css';
+import { Breadcrumb } from 'antd';
 import type { FC } from 'react';
+import { Link } from 'react-router';
 
 import { EditGroupFields } from '@features/edit-order-group';
+import { useCurrentOrderGroupID } from '@shared/lib';
 import { EditOrderWidget } from '@widgets/edit-order';
 
 const styles = {
   container: css`
     padding: 16px;
   `,
+  breadcrumbs: css`
+    margin-bottom: 16px;
+  `,
 };
 
 const OrderEditPage: FC = () => {
+  const { groupID } = useCurrentOrderGroupID();
+
   return (
     <div className={styles.container}>
+      <Breadcrumb
+        className={styles.breadcrumbs}
+        items={[
+          { title: <Link to="/">Заказы</Link> },
+          { title: `Заказ №${groupID}` },
+        ]}
+      />
       <EditGroupFields />
       <EditOrderWidget />
     </div>
