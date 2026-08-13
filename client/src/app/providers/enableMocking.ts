56124d3c0aa8506797c7085eccf19ab0fe4c0b20
tingoll <roma.oliynyk83@gmail.com>
2026-08-13 15:24:28 +0300

    client: add optional MSW dev mode

--- client/src/app/providers/enableMocking.ts
+++ client/src/app/providers/enableMocking.ts
@@ -1,5 +1,5 @@
 export async function enableMocking() {
-  if (!import.meta.env.DEV || import.meta.env.VITE_ENABLE_MSW === 'false') {
+  if (!import.meta.env.DEV || import.meta.env.VITE_ENABLE_MSW !== 'true') {
     return;
   }
 
